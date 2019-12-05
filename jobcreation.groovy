5714a3c8bc947a2b08d261f517142b6885b93693
rayinianji <anjaneyulu195@gmail.com>
2019-12-05 23:32:38 +0530

    updated groovy script

--- jobcreation.groovy
+++ jobcreation.groovy
@@ -2,3 +2,16 @@ folder('Webprojects') {
     displayName('ABC WebProject')
     description('Folder for project ABC')
 }
+
+job('SampleWebapp') {
+    scm {
+        git {
+		
+            remote {
+                url('https://github.com/rayinianji/NewWebproject.git')
+				
+                }
+			branch('master')	
+        }
+    }
+}
